0e866e8cb51fd24394cadc976ef6a04d7095daa8
Patricio Keilty <patricio.keilty@estudios-electricos.com>
2020-10-01 16:34:00 -0300

    Agregado logs en métodos del ciclo de vida de Activity.

--- TestActivity/app/src/main/java/ar/edu/ips/aus/android/test_activity/TestActivity.java
+++ TestActivity/app/src/main/java/ar/edu/ips/aus/android/test_activity/TestActivity.java
@@ -4,14 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class TestActivity extends Activity {
 
+	public static final String TEST_ACTIVITY = "TestActivity";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.i(TEST_ACTIVITY, "Llamada a OnCreate()");
 		setContentView(R.layout.activity_test);
 
 		Button btnExplicitCall = (Button) findViewById(R.id.button2);
@@ -45,5 +49,40 @@ public class TestActivity extends Activity {
 				startActivity(explIntent);
 			}
 		});
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.i(TEST_ACTIVITY, "Llamada a OnStart()");
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Log.i(TEST_ACTIVITY, "Llamada a OnRestart()");	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.i(TEST_ACTIVITY, "Llamada a OnResume()");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.i(TEST_ACTIVITY, "Llamada a OnPause()");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.i(TEST_ACTIVITY, "Llamada a OnStop()");
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.i(TEST_ACTIVITY, "Llamada a OnDestroy()");
 	}
 }
